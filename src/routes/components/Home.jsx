5304147b606c9c99651c88cc2db60d1c3f1a453e
Mushal Manzoor <113787105+mushalm@users.noreply.github.com>
2023-09-05 22:57:14 +0500

    update home

--- src/routes/components/Home.jsx
+++ src/routes/components/Home.jsx
@@ -1,4 +1,4 @@
-import {React, useState ,useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import ChatbotFrame from "./ChatbotFrame";
 import { useTranslation } from "react-i18next";
 export default function Home() {
@@ -473,22 +473,22 @@ export default function Home() {
                                   aria-label="Twitter Account"
                                   target="_blank"
                                   rel="noreferrer"
-                                  href="https://twitter.com/jacobandreou/status/1632423894832545794"
+                                  href="https://twitter.com/I4NW/status/1631353148903239680"
                                 >
                                   <svg
-                              width="16"
-                              height="16"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-                                fill="currentColor"
-                              ></path>
-                            </svg>
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      clipRule="evenodd"
+                                      d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                                      fill="currentColor"
+                                    ></path>
+                                  </svg>
                                 </a>
                               </div>
                               <blockquote className="text-gray-900">
@@ -498,16 +498,16 @@ export default function Home() {
                               </blockquote>
                               <figcaption className="mt-6 flex items-center gap-x-4">
                                 <img
-                                  className="h-10 w-10 rounded-full bg-gray-50"
-                                  src="https://pbs.twimg.com/profile_images/1642428370091925505/xknh7v7j_400x400.jpg"
+                                  className="h-10 w-10 flex-none rounded-full bg-gray-50"
+                                  src="https://pbs.twimg.com/profile_images/1257618949665173505/izjTnliK_400x400.jpg"
                                   alt=""
                                 />
                                 <div>
                                   <div className="font-semibold">
-                                  {t("Abdullah Abdulaziz")}
-                            </div>
-                            <div className="text-gray-600">
-                              {t("Chief Executive Officer")}
+                                    {t("Abdullah Abdulaziz")}
+                                  </div>
+                                  <div className="text-gray-600">
+                                    {t("Chief Executive Officer")}
                                   </div>
                                 </div>
                               </figcaption>
